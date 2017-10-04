7d63b2a06b6105f47098c944bdf6ac3c7de84471
David Arango <davidarango@Davids-MacBook-Pro.local>
2017-10-04 09:12:38 -0500

    Resolved image problem 404 gitHub error

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ $(document).ready(function() {
 // Hero image gallery with timer
 ////
 
-const heroImages = ['../beauty_pop/images/hero/hero.jpg', '../beauty_pop/images/hero/hero_one.jpg', '../beauty_pop/images/hero/hero_four.jpg'];
+const heroImages = ['../Beauty_Pop/images/hero/hero.jpg', '../Beauty_Pop/images/hero/hero_two.jpg', '../Beauty_Pop/images/hero/hero_four.jpg'];
 let heroCounter = 0;
 $('.site_header_right_button').on('click', () => {
   heroCounter++;
